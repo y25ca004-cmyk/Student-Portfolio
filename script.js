3022df05c77fda167d80568586b949182711a60e
y25ca004-cmyk <y25ca004@rvrjc.ac.in>
2026-08-27 16:38:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// Student Portfolio JavaScript
 
+console.log("Portfolio loaded successfully!");
+
+
+// Add shadow to navigation when scrolling
+
+window.addEventListener("scroll", function () {
+
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+
+        header.style.boxShadow =
+            "0 3px 15px rgba(0,0,0,0.3)";
+
+    } else {
+
+        header.style.boxShadow = "none";
+
+    }
+
+});
